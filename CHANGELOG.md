f131b21ea21167566a6b2086b253d881975106ab
Zhuinden <zhuinden@gmail.com>
2017-05-06 18:09:02 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Change log
+
+- Scope Manager x.x.x (yyyy-MM-dd)
+WIP
---
+++ scope-manager/src/test/java/com/zhuinden/scopemanager/ScopeManagerTest.java
@@ -0,0 +1,14 @@
+package com.zhuinden.scopemanager;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by Zhuinden on 2017.03.01..
+ */
+
+public class ScopeManagerTest {
+}
